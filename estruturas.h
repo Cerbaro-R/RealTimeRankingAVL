1d5740506a5634a4464b014361201355f26fa8da
CERBARO <cerbarorafael1@gmail.com>
2026-05-28 21:03:24 -0300

    arquivo que faltou

---
+++ estruturas.h
@@ -0,0 +1,12 @@
+#ifndef ESTRUTURAS_H
+#define ESTRUTURAS_H
+
+#include <string.h>
+
+// Estrutura que representa um jogador no sistema
+typedef struct {
+    char nome[50];
+    int pontuacao;
+} Jogador;
+
+#endif // ESTRUTURAS_H
